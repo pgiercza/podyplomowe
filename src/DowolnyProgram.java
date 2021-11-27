d9c5ddd97995588263ec0a761892cc58db74970f
pgiercza <pgiercza@gmail.com>
2021-11-27 14:36:23 +0100

    Uaktualnienie prokramu o komentarz i zmianę.

--- src/DowolnyProgram.java
+++ src/DowolnyProgram.java
@@ -1,6 +1,7 @@
 public class DowolnyProgram {
     public static void main(String[] args) {
         System.out.println("To jesst dowolny mało ambitny program.");
+        System.out.println("To jest kolejny krok... Uaktualnienie projektu");//komentarz
     }
 
 }
